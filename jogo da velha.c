c13dfe2a2ed9885da7a71899f1773b37b2a94b17
Aluno Feira <aluno.fsa@senaiba.com.br>
2025-05-15 20:41:04 -0300

    inserir caractere

--- jogo da velha.c
+++ jogo da velha.c
@@ -3,11 +3,11 @@
 
 
 char posicoes[3][3];
-char fully = 'A';
 
 
 void tabuleiroJogo(){
 	int i, j;
+	char fully = ' ';
 	
 	for(i=1; i<4; i++){
 		for (j=1; j<4; j++){
@@ -15,7 +15,6 @@ void tabuleiroJogo(){
 		}
 	}
 	
-	printf("Inicio\n\n");
 	printf(" %c | %c | %c \n", posicoes[1][1], posicoes[1][2], posicoes[1][3]);
 	printf("----------- \n");
 	printf(" %c | %c | %c \n",posicoes[2][1], posicoes[2][2], posicoes[2][3]);
@@ -24,19 +23,51 @@ void tabuleiroJogo(){
 	
 }
 
+void andamentoJogo(){
 	
+	printf(" %c | %c | %c \n", posicoes[1][1], posicoes[1][2], posicoes[1][3]);
+	printf("----------- \n");
+	printf(" %c | %c | %c \n",posicoes[2][1], posicoes[2][2], posicoes[2][3]);
+	printf("----------- \n");
+	printf(" %c | %c | %c \n",posicoes[3][1], posicoes[3][2], posicoes[3][3]);
+}
+
+void inserePlayer01(char p1){
+	int linha, coluna=0;
+	
+	printf("Player 01, escolha a linha e a coluna\n");
+	scanf("%i", &linha);
+	scanf("%i", &coluna);
+	posicoes[linha][coluna]=p1;
+}
+
+void inserePlayer02(char p2){
+	int linha, coluna=0;
+	
+	printf("Player 02, escolha a linha e a coluna\n");
+	scanf("%i", &linha);
+	scanf("%i", &coluna);
+	posicoes[linha][coluna]=p2;
+}
+
 int main(int argc, char *argv[]) {
 	
-	char escolha[2];
+	char player1, player2;
 	
+ 		 printf("Player 01, escolha 'X' ou 'O'\n");
+  		 scanf(" %c", &player1);
+	 	 printf("Player 02, escolha 'X' ou 'O'\n");
+  	 	 scanf(" %c", &player2);
+	  	 tabuleiroJogo();
+		 printf("Inicio de jogo\n");
+		 				 
 	do{
-	
-		printf("\nPlayer 01, Voce quer jogar com X ou 0?\n");
-		scanf("%s", &escolha[1]);
-		printf("Player 02, Voce quer jogar com X ou 0?\n");
-		scanf("%s", &escolha[2]);
-		tabuleiroJogo();
 		
-	}while(escolha!=3);
+		inserePlayer01(player1);
+		andamentoJogo();
+		inserePlayer02(player2);
+		andamentoJogo();
+						
+	}while(1);
 
 }
