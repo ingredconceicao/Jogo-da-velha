59716e1b2440e3843238ae7c933e1bce613b3c92
ingredconceicao <aluno.fsa@senaiba.com.br>
2025-05-16 18:55:50 -0300

    verifica caracteres

--- jogo da velha.c
+++ jogo da velha.c
@@ -34,33 +34,76 @@ void andamentoJogo(){
 
 void inserePlayer01(char p1){
 	int linha, coluna=0;
+	int quit=0;
 	
-	printf("Player 01, escolha a linha e a coluna\n");
-	scanf("%i", &linha);
-	scanf("%i", &coluna);
-	posicoes[linha][coluna]=p1;
+	while(quit==0){
+		printf("Player 01, escolha a linha e a coluna\n");
+		scanf("%i", &linha);
+		scanf("%i", &coluna);
+			if(posicoes[linha][coluna]!=' '){
+				printf("Posicao preenchida. Escolha outra\n");
+			}else{
+				posicoes[linha][coluna]=p1;
+				quit=1;
+			}
+	}
+	
 }
 
 void inserePlayer02(char p2){
 	int linha, coluna=0;
+	int quit=0;
 	
-	printf("Player 02, escolha a linha e a coluna\n");
-	scanf("%i", &linha);
-	scanf("%i", &coluna);
-	posicoes[linha][coluna]=p2;
+	while(quit==0){
+		printf("Player 02, escolha a linha e a coluna\n");
+		scanf("%i", &linha);
+		scanf("%i", &coluna);
+			if(posicoes[linha][coluna]!=' '){
+				printf("Posicao preenchida. Escolha outra\n");
+			}else{
+				posicoes[linha][coluna]=p2;
+				quit=1;
+			}
+	}
+
 }
 
 int main(int argc, char *argv[]) {
 	
 	char player1, player2;
+	int saida=0;
 	
- 		 printf("Player 01, escolha 'X' ou 'O'\n");
+	while(saida==0){
+		printf("Player 01, escolha 'X' ou 'O'\n");
   		 scanf(" %c", &player1);
-	 	 printf("Player 02, escolha 'X' ou 'O'\n");
-  	 	 scanf(" %c", &player2);
-	  	 tabuleiroJogo();
-		 printf("Inicio de jogo\n");
-		 				 
+			switch(player1){
+				case 'X':
+					printf("Player 02 jogara com 'O'\n");
+					player2='O';
+					saida=1;
+					break;
+				case 'x':
+					printf("Player 02 jogara com 'o'\n");
+					player2='o';
+					saida=1;
+					break;
+				case 'O':
+					printf("Player 02 jogara com 'X'\n");
+					player2='X';
+					saida=1;
+					break;
+				case 'o':
+					printf("Player 02 jogara com 'x'\n");
+					player2='o';
+					saida=1;
+					break;
+				default:
+					printf("Caractere Invalido. Escolha 'X' ou 'O'\n");
+					break;
+			}
+	}	
+ 		tabuleiroJogo();
+		printf("Inicio de jogo\n");				 
 	do{
 		
 		inserePlayer01(player1);
@@ -68,6 +111,6 @@ int main(int argc, char *argv[]) {
 		inserePlayer02(player2);
 		andamentoJogo();
 						
-	}while(1);
+	}while(saida==1);
 
 }
